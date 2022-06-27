583d596c9031c572c24b8d430952891742a0e54d
Алтынбек Турсункулов <aaa.altyn99@mail.ru>
2022-06-27 12:08:14 +0600

    fix

--- Quiz_game/Program.cs
+++ Quiz_game/Program.cs
@@ -1,8 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-string? Name;
+﻿string? Name;
 char?  answer = null;
 bool validity_flag = false;
 int point = 0;
+
 Console.WriteLine("What is your name?");
 Name = Console.ReadLine();
 Console.WriteLine("Hello, " + Name + "! Welcome to the quiz!");
@@ -13,8 +13,14 @@ Console.WriteLine("А:Russia");
 Console.WriteLine("B:Ukraine");
 Console.WriteLine("C:Belarus");
 answer = Convert.ToChar(Console.ReadLine());
-if (answer == 'B' || answer == 'b') { Console.WriteLine("Correctly!"); validity_flag = true; point++; }
-else Console.WriteLine("Wrong answer!"); validity_flag = false;
+if (answer == 'B' || answer == 'b') 
+{
+    Console.WriteLine("Correctly!"); 
+    validity_flag = true; 
+    point++; 
+}
+else Console.WriteLine("Wrong answer!"); 
+validity_flag = false;
 
 Console.WriteLine("Second question!");
 Console.WriteLine("Whose national dish is cabbage soup?");
@@ -23,8 +29,14 @@ Console.WriteLine("А:Russia");
 Console.WriteLine("B:Ukraine");
 Console.WriteLine("C:Belarus");
 answer = Convert.ToChar(Console.ReadLine());
-if (answer == 'A' || answer == 'a') { Console.WriteLine("Correctly!"); validity_flag = true; point++; }
-else Console.WriteLine("Wrong answer!"); validity_flag = false;
+if (answer == 'A' || answer == 'a') 
+{ 
+    Console.WriteLine("Correctly!"); 
+    validity_flag = true; 
+    point++; 
+}
+else Console.WriteLine("Wrong answer!"); 
+validity_flag = false;
 
 Console.WriteLine("Third question!");
 Console.WriteLine("Whose national dish is Draniki?");
@@ -33,11 +45,17 @@ Console.WriteLine("А:Russia");
 Console.WriteLine("B:Ukraine");
 Console.WriteLine("C:Belarus");
 answer = Convert.ToChar(Console.ReadLine());
-if (answer == 'C' || answer == 'c') { Console.WriteLine("Correctly!"); validity_flag = true; point++; }
-else Console.WriteLine("Wrong answer!"); validity_flag = false;
-if (point == 3) { Console.WriteLine("Congratulations on the victory! "+ Name); }
+if (answer == 'C' || answer == 'c') 
+{ 
+    Console.WriteLine("Correctly!"); 
+    validity_flag = true; 
+    point++; 
+}
+else Console.WriteLine("Wrong answer!"); 
+validity_flag = false;
+
+if (point == 3) 
+{ 
+    Console.WriteLine("Congratulations on the victory! "+ Name); 
+}
 else Console.WriteLine("Sorry you are dumb, close the game! "+ Name);
-
-
-
-
